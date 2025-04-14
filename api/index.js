c37fe6ade2dc4800b5fba09134e453f269b3b158
7void <kagisorabada12345@gmail.com>
2025-04-15 02:02:22 +0530

    wannabe reddit pt5

---
+++ api/index.js
@@ -0,0 +1,5 @@
+// api/index.js
+require("dotenv").config();
+const express = require("server\server.js");
+// … all your middleware/routes …
+module.exports = app;
--- api/server.js
+++
@@ -1,5 +0,0 @@
-// api/server.js
-const app = require("server\server.js"); // Adjust the path if your main file is different
-
-// Export the Express app as the serverless function handler
-module.exports = app;
